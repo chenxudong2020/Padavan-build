a9c95bf4f70ce9f7079314d69d8cbdfb0bdbf469
chenxudong2020 <872603935@qq.com>
2023-01-22 19:15:38 +0800

    add K2P 4.4

--- padavan/K2P-NANO/K2P-NANO.sh
+++ padavan/K2P-NANO/K2P-NANO.sh
@@ -3,7 +3,7 @@
 
 
 
-DESTDIR=/opt/rt-n56u
+DESTDIR=/opt/padavan
 # echo "--------------开始复制默认配置文件----------"
 # \cp -f ../public/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
 #  echo "--------------开始复制默认配置文件结束----------"
@@ -14,7 +14,15 @@ DESTDIR=/opt/rt-n56u
 # cp -rf ../public/boards/. ${DESTDIR}/trunk/configs/boards
 #\cp -f ../public/JDC-1.config ${DESTDIR}/trunk/configs/templates
 # echo "--------------复制JDC1配置文件结束----------"
- 
+
+
+ default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
+
+    echo "--------------开始复制主题----------------------"
+	sudo rm -rf $default_theme_path/bootstrap
+	sudo rm -rf $default_theme_path/images
+	cp -rf ../public/theme/. $default_theme_path
+	echo "--------------复制主题文件结束------------------"
 
 
 
